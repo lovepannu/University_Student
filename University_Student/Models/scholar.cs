06078b14c1f69f7822166b6fbdbc9135b9d2b663
lovepannu <72423124+lovepannu@users.noreply.github.com>
2021-04-19 14:01:24 +1200

    Changes in the folder

--- University_Student/Models/scholar.cs
+++ University_Student/Models/scholar.cs
@@ -9,10 +9,15 @@ namespace University_Student.Models
     public class scholar
     {
         [Key]
+        This is the primary key
         public int Id { get; set; }
+         //This is the variable to store the student name
         public string student_Name { get; set; }
+         //This is the variable to store the student email
         public string student_Email { get; set; }
+         //This is the variable to store the student mobile
         public string student_Mobile { get; set; }
+         //This is the variable to store the course name
         public string course_Name { get; set; }
     }
 }
